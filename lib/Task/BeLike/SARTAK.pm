4e6cdde517095b263f17e752620429f462f3fc9c
Shawn M Moore <sartak@bestpractical.com>
2011-06-14 20:30:52 -0400

    Shell of a pm file

---
+++ lib/Task/BeLike/SARTAK.pm
@@ -0,0 +1,29 @@
+package Task::BeLike::SARTAK;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Task::BeLike::SARTAK
+
+=head1 DESCRIPTION
+
+See the F<Makefile.PL> in this distribution.
+
+=head1 AUTHOR
+
+Shawn M Moore C<sartak@gmail.com>
+
+=head1 COPYRIGHT
+
+Copyright 2011 Shawn M Moore.
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
+
