3f8c6b6f84dca12a364c0c0fac005efc6da35555
Shawn M Moore <code@sartak.org>
2012-05-30 08:13:39 -0400

    Copyright and email

--- lib/Task/BeLike/SARTAK.pm
+++ lib/Task/BeLike/SARTAK.pm
@@ -17,11 +17,11 @@ See the F<Makefile.PL> in this distribution.
 
 =head1 AUTHOR
 
-Shawn M Moore C<sartak@gmail.com>
+Shawn M Moore C<code@sartak.org>
 
 =head1 COPYRIGHT
 
-Copyright 2011 Shawn M Moore.
+Copyright 2011-2012 Shawn M Moore.
 
 This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
